1132ab709404d9cb002bbe4cc94b89c36dd95c3a
ZaurO5 <165468417+ZaurO5@users.noreply.github.com>
2024-11-06 23:29:01 +0400

    Website plus Admin full added(before slider)

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zay_Shop.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/ShopController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zay_Shop.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
